eb63e38e08fff6e21e0a991e46101d6eaacc46e4
Jared Porcenaluk <jared@NebbiaTech.com>
2017-12-13 08:01:18 -0500

    Sending message through queue.

--- ServiceBus.NET.Library/Messaging.cs
+++ ServiceBus.NET.Library/Messaging.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Azure.ServiceBus;
+using System.Threading.Tasks;
 
 namespace ServiceBus.NET.Library
 {
     public class Messaging
     {
-        public string SendMessage(string message)
+        const string ServiceBusConnectionString = "Endpoint=sb://taxslayertest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=bGbZ1lXAI7FCjmyYusoeP++g1SLT0AQA/zkWSci5Lp8=";
+        const string QueueName = "testqueue";
+        static IQueueClient queueClient;
+
+        public Messaging()
         {
-            return "Not Implemented";
+
         }
+
+        public async Task<string> SendMessage(string messageBody)
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            Console.WriteLine($"Sending message: {messageBody}");
+
+            await queueClient.SendAsync(message);
+
+            await queueClient.CloseAsync();
+
+            //TODO: Richer response
+            return "Message Sent Successfully";
+        }
+
+        //TODO: Receive messages, allow subscription of
+
     }
 }
